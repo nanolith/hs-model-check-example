e89a15ba9260852fb7103bbc92c5f90e3c94db1d
Justin Handville <nanolith@gmail.com>
2026-08-14 15:07:29 -0400

    RelationalExpression data type.

--- CalculatorDSL.hs
+++ CalculatorDSL.hs
@@ -8,3 +8,13 @@ data Expression =
     | Multiplication Expression Expression
     | Divide Expression Expression
     deriving (Eq, Show)
+
+-- Relational expressions
+data RelationalExpression =
+      Equal Expression Expression
+    | NotEqual Expression Expression
+    | LessThan Expression Expression
+    | LessThanEqual Expression Expression
+    | GreaterThan Expression Expression
+    | GreaterThanEqual Expression Expression
+    deriving (Eq, Show)
