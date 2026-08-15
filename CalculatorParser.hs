89611d12ca39f6acebeb86c3655bb09078d4af3b
Justin Handville <nanolith@gmail.com>
2026-08-15 19:36:27 -0400

    spaceConsumer parser combinator.

--- CalculatorParser.hs
+++ CalculatorParser.hs
@@ -3,6 +3,16 @@ module CalculatorParser where
 import Data.Text (Text)
 import Data.Void (Void)
 import Text.Megaparsec
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
 
 -- the parser type
 type Parser = Parsec Void Text
+
+-- Consume spaces
+spaceConsumer :: Parser ()
+spaceConsumer =
+    L.space
+        space1
+        (L.skipLineComment "//")
+        (L.skipBlockComment "/*" "*/")
